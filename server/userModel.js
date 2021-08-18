ca781f8639ce6f76a821adb751173f91f1005b51
Calvin Li <calvinli12345@gmail.com>
2021-08-18 11:43:52 -0500

    added user model

---
+++ server/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema ({
+    username: {
+        type: String,
+        trim: true,
+        required: "Username is Required"
+    },
+
+    password: {
+        type: String,
+        trim: true,
+        required: "Password is Required",
+        validate: [({ length }) => length >= 5, "Password needs to be longer."]
+    },
+
+    email: {
+        type: String,
+        unique: true,
+        required: "Email is Required",
+        match: [/.+@.+\..+/, "Please enter a valid email address"]
+    }
+    });
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
