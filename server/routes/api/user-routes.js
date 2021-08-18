a1c8d3f9a9c68a1d0568142f2087fa8510e96a2f
Zacharyjg <guldb016@umn.edu>
2021-08-18 14:10:32 -0500

    Added in authMiddleware

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -3,7 +3,11 @@ const router = require('express').Router();
 const {
     createUser,
     getUser,
+    login,
 } = require("../../controllers/user-controller");
 
-router.route("/dashboard").get(getUser);
-router.route("/login").post(createUser);
+const { authMiddleware } = require('../../utils/auth');
+
+router.route("/dashboard").get(authMiddleware, getUser);
+router.route("/login").post(login);
+router.route("/signup").post(createUser);
