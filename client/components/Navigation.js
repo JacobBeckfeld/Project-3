d17f046f5167f8458c85bfe68aa45defb0f94317
neilburt <neil.burt@comcast.net>
2021-08-18 16:35:11 -0500

    Navigation.js scaffold, BS CDN in index.html, client auth.js & .gitignore, updated App.js & index.js.

---
+++ client/components/Navigation.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Auth from '../src/utils/auth';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/index.css';
+
+export default function Navigation() {
+
+  function showNavigation() {
+    if (Auth.loggedIn()) {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/dashboard">
+              Dashboard
+            </Link>
+          </li>
+          <li className="mx-1">
+            <a href="/" onClick={() => Auth.logout()}>
+              Logout
+            </a>
+          </li>
+        </ul>
+      )
+    } else {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/login">
+              Login / Signup
+            </Link>
+          </li>
+        </ul>
+      )
+    }
+  }
+
+  return (
+    <header className="flex-row px-1">
+      <h1>
+        <Link to="/">
+          Home
+        </Link>
+      </h1>
+
+      <nav>
+        {showNavigation()}
+      </nav>
+    </header>
+  )
+}
