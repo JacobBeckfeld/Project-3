cb7bfc92d0ce2f01c5b62ed8750ec065ff01f69e
Calvin Li <calvinli12345@gmail.com>
2021-08-19 14:31:50 -0500

    added search page to nav and rewrote search page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,29 +1,36 @@
 import React from "react";
 import { Jumbotron, Container, Card, CardTitle, CardDeck, 
-  CardSubtitle, CardBody, ListGroup, ListGroupItem } from 'reactstrap';
+CardSubtitle, CardBody, ListGroup, ListGroupItem } from 'reactstrap';
 
-const mainProfile = () => {
-    return (
-      <div>
-        <Jumbotron fluid>
-          <Container fluid>
-            <h1 className="display-3">Hello, </h1>
-            <p class="lead">Paragon Level:  </p>
-            <p class="lead">Guild:</p>
-          </Container>
-        </Jumbotron>
-      </div>
-    );
-  };
+import { getProfile } from "../utils/API";
 
-const allChars = () => {
-    return (
-        <CardDeck>
+const handleSearch = async () => {
+  try {
+      const results = await (await getProfile("Laserrpg999#1705")).json();
+      console.log(results)
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+const Search = (props) => {
+  handleSearch();
+  return (
+    <div>
+      <Jumbotron fluid>
+        <Container fluid>
+          <h1 className="display-3">Hello, {props.battleTag} </h1>
+          <p class="lead">Paragon Level:  </p>
+          <p class="lead">Guild:</p>
+        </Container>
+      </Jumbotron>
+      <CardDeck>
         <Card>
             <CardBody>
             <CardTitle tag="h5"></CardTitle>
             <CardSubtitle tag="h6" className="mb-2 text-muted"></CardSubtitle>
             <ListGroup>
+                <h1 class="text-center"> Heros </h1>
                 <ListGroupItem>Level: </ListGroupItem>
                 <ListGroupItem>Paragon Level: </ListGroupItem>
                 <ListGroupItem>Hardcore?: </ListGroupItem>
@@ -32,7 +39,27 @@ const allChars = () => {
             </CardBody>
         </Card>
         </CardDeck>
-    );
+    </div>
+  );
 };
 
-export default { mainProfile, allChars };
+// const allChars = () => {
+//     return (
+//         <CardDeck>
+//         <Card>
+//             <CardBody>
+//             <CardTitle tag="h5"></CardTitle>
+//             <CardSubtitle tag="h6" className="mb-2 text-muted"></CardSubtitle>
+//             <ListGroup>
+//                 <ListGroupItem>Level: </ListGroupItem>
+//                 <ListGroupItem>Paragon Level: </ListGroupItem>
+//                 <ListGroupItem>Hardcore?: </ListGroupItem>
+//                 <ListGroupItem>Season?: </ListGroupItem>
+//             </ListGroup>
+//             </CardBody>
+//         </Card>
+//         </CardDeck>
+//     );
+// };
+
+export default Search
