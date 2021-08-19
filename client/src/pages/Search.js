79d5dc3416745e9ad45914ebdd653374f3f09cb0
Zacharyjg <guldb016@umn.edu>
2021-08-19 10:59:38 -0500

    Fixed issue where we had extra pages folder outside of src

---
+++ client/src/pages/Search.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Jumbotron, Container, Card, CardTitle, CardDeck, 
+  CardSubtitle, CardBody, ListGroup, ListGroupItem } from 'reactstrap';
+
+const mainProfile = () => {
+    return (
+      <div>
+        <Jumbotron fluid>
+          <Container fluid>
+            <h1 className="display-3">Hello, </h1>
+            <p class="lead">Paragon Level:  </p>
+            <p class="lead">Guild:</p>
+          </Container>
+        </Jumbotron>
+      </div>
+    );
+  };
+
+const allChars = () => {
+    return (
+        <CardDeck>
+        <Card>
+            <CardBody>
+            <CardTitle tag="h5"></CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted"></CardSubtitle>
+            <ListGroup>
+                <ListGroupItem>Level: </ListGroupItem>
+                <ListGroupItem>Paragon Level: </ListGroupItem>
+                <ListGroupItem>Hardcore?: </ListGroupItem>
+                <ListGroupItem>Season?: </ListGroupItem>
+            </ListGroup>
+            </CardBody>
+        </Card>
+        </CardDeck>
+    );
+};
+
+export default mainProfile;
+export default allChars;
