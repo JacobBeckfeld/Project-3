a2affb0010fb5db5f96197f59a2621be529b723f
Zacharyjg <guldb016@umn.edu>
2021-08-20 10:39:09 -0500

    Added code to generate an access token using oauth

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,3 +1,20 @@
+let key = "000ecf7460764ad49dc126af35cb090b";
+let secret = "5T46w93U6ucQZ677R4uiidPdeHlEajSi";
+
+const getToken = async () => {
+    const results = await fetch('https://us.battle.net/oauth/token', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: 'grant_type=client_credentials&client_id=' + key + '&client_secret=' + secret
+    });
+
+    return await results.json();
+}
+
+const accessToken = getToken();
+
 export const getUser = (token) => {
     return fetch("/api/users/dashboard", {
         headers: {
@@ -28,25 +45,25 @@ export const loginUser = (userData) => {
 };
 
 export const getProfile = (battletag) => {
+
     let newBattletag = battletag.replace(/#/, "%23");
 
-    return fetch(`https://us.api.blizzard.com/d3/profile/${newBattletag}/?locale=en_US&access_token=US9BodSM03UzyVr9cxr7HLSvLeQGC5i9Cc`);
-
-    // return results.json();
-
-    // return fetch(`https://us.api.blizzard.com/d3/profile/${newBattletag}/?locale=en_US&access_token=US9BodSM03UzyVr9cxr7HLSvLeQGC5i9Cc`)
-    // .then(function (res) {
-    //     return res.json();
-    // })
-    // .then(function (data) {
-    //     return data;
-    // });
-    // "https://us.api.blizzard.com/d3/profile/Laserrpg999%231705/?locale=en_US&access_token=US9BodSM03UzyVr9cxr7HLSvLeQGC5i9Cc"
+    return fetch(`https://us.api.blizzard.com/d3/profile/${newBattletag}/?locale=en_US&access_token=${accessToken}`);
 }
 
 export const getCharacter = (battletag, heroID) => {
 
     let newBattletag = battletag.replace(/#/, "%23");
 
-    return fetch(`https://us.api.blizzard.com/d3/profile/${newBattletag}/hero/${heroID}?locale=en_US&access_token=US9BodSM03UzyVr9cxr7HLSvLeQGC5i9Cc`)
+    return fetch(`https://us.api.blizzard.com/d3/profile/${newBattletag}/hero/${heroID}?locale=en_US&access_token=${accessToken}`)
+}
+
+export const getToken = () => {
+    return fetch('https://us.battle.net/oauth/token', {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Basic ' + btoa('000ecf7460764ad49dc126af35cb090b:5T46w93U6ucQZ677R4uiidPdeHlEajSi')
+        },
+        body: 'grant_type=client_credentials'
+    });
 }
