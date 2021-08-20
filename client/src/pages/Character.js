1e76672b7b360ad6634440e72d7c2b70195b03f1
JacobBeckfeld <jacbeck456@gmail.com>
2021-08-20 10:41:21 -0500

    fixed rendering

--- client/src/pages/Character.js
+++ client/src/pages/Character.js
@@ -44,7 +44,6 @@ const Character = () => {
     return (
         <div>
             <Card>
-                <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
                 <CardBody>
                     <CardTitle tag="h5">Character Name:{hero.name}</CardTitle>
                     <CardSubtitle tag="h6" className="mb-2 text-muted">Character Class: {hero.class}</CardSubtitle>
@@ -58,41 +57,41 @@ const Character = () => {
                     <CardBody>
                         
                         <CardTitle tag="h5">Skill List</CardTitle>
-                        <CardSubtitle>{hero.skills.active[0].skill.name}:</CardSubtitle>
-                        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
-                        <CardText>{hero.skills.active[0].skill.description}</CardText>
-                        <CardSubtitle>Rune: {hero.skills.active[0].rune.name}</CardSubtitle>
-                        <CardText>{hero.skills.active[0].rune.description}</CardText>
+                        <CardSubtitle>{hero.skills ? hero.skills.active[0].skill.name : ''}:</CardSubtitle>
+                        <CardImg top width="100%" src={hero.skills ? `http://media.blizzard.com/d3/icons/${hero.skills.active[0].skill.icon}/small/<icon>.png` : ""} alt="Card image cap" />
+                        <CardText>{hero.skills ? hero.skills.active[0].skill.description : ""}</CardText>
+                        <CardSubtitle>Rune: {hero.skills ? hero.skills.active[0].rune.name : ""}</CardSubtitle>
+                        <CardText>{hero.skills ? hero.skills.active[0].rune.description : ""}</CardText>
                         
-                        <CardSubtitle>{hero.skills.active[1].skill.name}:</CardSubtitle>
+                        <CardSubtitle>{hero.skills ? hero.skills.active[1].skill.name : ""}:</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
-                        <CardText>{hero.skills.active[1].skill.description}</CardText>
-                        <CardSubtitle>Rune: {hero.skills.active[1].rune.name}</CardSubtitle>
-                        <CardText>{hero.skills.active[1].rune.description}</CardText>
+                        <CardText>{hero.skills ? hero.skills.active[1].skill.description : ""}</CardText>
+                        <CardSubtitle>Rune: {hero.skills ? hero.skills.active[1].rune.name : ""}</CardSubtitle>
+                        <CardText>{hero.skills ? hero.skills.active[1].rune.description : ""}</CardText>
                         
-                        <CardSubtitle>{hero.skills.active[2].skill.name}:</CardSubtitle>
+                        <CardSubtitle>{hero.skills ? hero.skills.active[2].skill.name : ""}:</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
-                        <CardText>{hero.skills.active[2].skill.description}</CardText>
-                        <CardSubtitle>Rune: {hero.skills.active[2].rune.name}</CardSubtitle>
-                        <CardText>{hero.skills.active[2].rune.description}</CardText>
+                        <CardText>{hero.skills ? hero.skills.active[2].skill.description : ""}</CardText>
+                        <CardSubtitle>Rune: {hero.skills ? hero.skills.active[2].rune.name : ""}</CardSubtitle>
+                        <CardText>{hero.skills ? hero.skills.active[2].rune.description : ""}</CardText>
                         
-                        <CardSubtitle>{hero.skills.active[3].skill.name}:</CardSubtitle>
+                        <CardSubtitle>{hero.skills ? hero.skills.active[3].skill.name : ""}:</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
-                        <CardText>{hero.skills.active[3].skill.description}</CardText>
-                        <CardSubtitle>Rune: {hero.skills.active[3].rune.name}</CardSubtitle>
-                        <CardText>{hero.skills.active[3].rune.description}</CardText>
+                        <CardText>{hero.skills ? hero.skills.active[3].skill.description : ""}</CardText>
+                        <CardSubtitle>Rune: {hero.skills ? hero.skills.active[3].rune.name : ""}</CardSubtitle>
+                        <CardText>{hero.skills ? hero.skills.active[3].rune.description : ""}</CardText>
                         
-                        <CardSubtitle>{hero.skills.active[4].skill.name}:</CardSubtitle>
+                        <CardSubtitle>{hero.skills ? hero.skills.active[4].skill.name : ""}:</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
-                        <CardText>{hero.skills.active[4].skill.description}</CardText>
-                        <CardSubtitle>Rune: {hero.skills.active[4].rune.name}</CardSubtitle>
-                        <CardText>{hero.skills.active[4].rune.description}</CardText>
+                        <CardText>{hero.skills ? hero.skills.active[4].skill.description : ""}</CardText>
+                        <CardSubtitle>Rune: {hero.skills ? hero.skills.active[4].rune.name: ""}</CardSubtitle>
+                        <CardText>{hero.skills ? hero.skills.active[4].rune.description : ""}</CardText>
                         
-                        <CardSubtitle>{hero.skills.active[5].skill.name}:</CardSubtitle>
+                        <CardSubtitle>{hero.skills ? hero.skills.active[5].skill.name : ""}:</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
-                        <CardText>{hero.skills.active[5].skill.description}</CardText>
-                        <CardSubtitle>Rune: {hero.skills.active[5].rune.name}</CardSubtitle>
-                        <CardText>{hero.skills.active[5].rune.description}</CardText> 
+                        <CardText>{hero.skills ? hero.skills.active[5].skill.description : ""}</CardText>
+                        <CardSubtitle>Rune: {hero.skills ? hero.skills.active[5].rune.name : ""}</CardSubtitle>
+                        <CardText>{hero.skills ? hero.skills.active[5].rune.description : ""}</CardText> 
 
                     </CardBody>
                 </Card>
@@ -100,43 +99,43 @@ const Character = () => {
 
                     <CardBody>
                         <CardTitle tag="h5">Equipment</CardTitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Head: {hero.items.head.name}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Head: {hero.items ? hero.items.head.name : ""}</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
                         
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Neck: {hero.items.neck.name}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Neck: {hero.items ? hero.items.neck.name : ""}</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
                         
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Torso: {hero.items.torso.name}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Torso: {hero.items ? hero.items.torso.name : ""}</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
                         
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Shoulders: {hero.items.shoulders.name}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Shoulders: {hero.items ? hero.items.shoulders.name : ""}</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
                         
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Legs: {hero.items.legs.name}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Legs: {hero.items ? hero.items.legs.name : ""}</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
                         
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Waist: {hero.items.waist.name}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Waist: {hero.items ? hero.items.waist.name : ""}</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
                         
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Hands: {hero.items.hands.name}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Hands: {hero.items ? hero.items.hands.name : ""}</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
                         
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Bracers: {hero.items.bracers.name}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Bracers: {hero.items ? hero.items.bracers.name : ""}</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
                         
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Feet: {hero.items.feet.name}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Feet: {hero.items ? hero.items.feet.name : ""}</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
                         
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Left Finger: {hero.items.leftFinger.name}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Left Finger: {hero.items ? hero.items.leftFinger.name : ""}</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
                         
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Right Finger: {hero.items.rightFinger.name}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Right Finger: {hero.items ? hero.items.rightFinger.name : ""}</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
                         
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Main Hand: {hero.items.mainHand.name}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Main Hand: {hero.items ? hero.items.mainHand.name : ""}</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
                         
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Off Hand: {hero.items.offHand.name}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Off Hand: {hero.items ? hero.items.offHand.name : ""}</CardSubtitle>
                         <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
                         
                     </CardBody>
@@ -146,28 +145,28 @@ const Character = () => {
                 <Card>
                     <CardBody>
                         <CardTitle tag="h5">Character Stats:</CardTitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Life: {hero.stats.life}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Damage: {hero.stats.damage}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Toughness: {hero.stats.toughness}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Healing: {hero.stats.healing}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Attack Speed: {hero.stats.attackSpeed}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Block Chance: {hero.stats.blockChance}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Armor: {hero.stats.armor}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Strength: {hero.stats.strength}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Dexterity: {hero.stats.dexterity}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Vitality: {hero.stats.vitality}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Intelligence: {hero.stats.intelligence}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Physical Resist: {hero.stats.physicalResist}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Fire Resist: {hero.stats.fireResist}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Cold Resist: {hero.stats.coldResist}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Lightning Resist: {hero.stats.lightningResist}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Poison Resist: {hero.stats.poisonResist}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Arcane Resist: {hero.stats.arcaneResist}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Crit Chance: {hero.stats.critChance}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Thorns: {hero.stats.thorns}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Life Steal: {hero.stats.lifeSteal}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Life per Kill: {hero.stats.lifePerKill}</CardSubtitle>
-                        <CardSubtitle tag="h6" className="mb-2 text-muted">Life on Hit: {hero.stats.lifeOnHit}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Life: {hero.stats ? hero.stats.life : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Damage: {hero.stats ? hero.stats.damage : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Toughness: {hero.stats ? hero.stats.toughness : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Healing: {hero.stats ? hero.stats.healing : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Attack Speed: {hero.stats ? hero.stats.attackSpeed : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Block Chance: {hero.stats ? hero.stats.blockChance : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Armor: {hero.stats ? hero.stats.armor : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Strength: {hero.stats ? hero.stats.strength : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Dexterity: {hero.stats ? hero.stats.dexterity : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Vitality: {hero.stats ? hero.stats.vitality : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Intelligence: {hero.stats ? hero.stats.intelligence : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Physical Resist: {hero.stats ? hero.stats.physicalResist : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Fire Resist: {hero.stats ? hero.stats.fireResist : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Cold Resist: {hero.stats ? hero.stats.coldResist : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Lightning Resist: {hero.stats ? hero.stats.lightningResist : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Poison Resist: {hero.stats ? hero.stats.poisonResist : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Arcane Resist: {hero.stats ? hero.stats.arcaneResist : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Crit Chance: {hero.stats ? hero.stats.critChance : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Thorns: {hero.stats ? hero.stats.thorns : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Life Steal: {hero.stats ? hero.stats.lifeSteal : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Life per Kill: {hero.stats ? hero.stats.lifePerKill : ""}</CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Life on Hit: {hero.stats ? hero.stats.lifeOnHit : ""}</CardSubtitle>
                     </CardBody>
                 </Card>
             </CardDeck>
