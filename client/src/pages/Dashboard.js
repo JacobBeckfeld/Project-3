1dc7725f01f787d23825af90f28b6e14fb0d7ca0
Zacharyjg <guldb016@umn.edu>
2021-08-21 11:32:42 -0500

    Added the getting of User data and set up rendering based on data

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,34 +2,39 @@ import { React, useState, useEffect } from "react";
 import { getProfile, getToken } from "../utils/API";
 import { Jumbotron, Card, ListGroup, ListGroupItem } from 'reactstrap';
 import Navigation from "../components/Navigation";
+import Auth from "../utils/auth";
 
 const Dashboard = (props) => {
+
     const [profile, setProfile] = useState({})
+
+    const User = Auth.getProfile().data;
+
+    console.log(User);
+
     const onPageLoad = async () => {
-        if (props.battletag) {
+        if (User.battletag) {
             try {
                 const token = await getToken();
-                const response = await (await getProfile(props.battletag, token)).json();
+                const response = await (await getProfile(User.battletag, token)).json();
                 console.log(response);
                 setProfile(response);
             } catch (error) {
                 console.log(error);
             }
         }
-
-
     }
 
     useEffect(() => {
         onPageLoad()
-    }, [])
+    }, []);
 
     return (
         <div>
             <Navigation />
             <Jumbotron>
                 <h1>Hello **CURRENT_USERNAME_HERE**</h1>
-                {props.battletag ?
+                {User.battletag != "" ?
                     profile.heroes.map((hero) => {
                         <Card>
                             <ListGroup>
