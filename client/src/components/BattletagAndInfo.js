0397e932300bbde42027cc9b83a08cea073a2adc
Zacharyjg <guldb016@umn.edu>
2021-08-21 16:57:32 -0500

    Added in component for displaying Battletag and paragon levels

---
+++ client/src/components/BattletagAndInfo.js
@@ -0,0 +1,21 @@
+import { React } from "react";
+import { Container } from 'reactstrap';
+
+
+
+const BattletagAndInfo = (props) => {
+    const profile = props.profile;
+
+    return (
+        <Container fluid>
+            <h1 className="display-3">Battletag: {profile.battleTag} </h1>
+            <p className="lead">Paragon Level: {profile.paragonLevel}  </p>
+            <p className="lead">Hardcore Paragon Level: {profile.paragonLevelHardcore}  </p>
+            <p className="lead">Season Paragon Level: {profile.paragonLevelSeason}  </p>
+            <p className="lead">Hardcore Season Paragon Level: {profile.paragonLevelSeasonHardcore}  </p>
+            <p className="lead">Guild: {profile.guildName} </p>
+        </Container>
+    );
+}
+
+export default BattletagAndInfo;
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,8 +1,11 @@
 import { React, useState, useEffect } from "react";
 import { getProfile, getToken } from "../utils/API";
-import { Jumbotron, Container } from 'reactstrap';
+import { Jumbotron } from 'reactstrap';
+
 import Navigation from "../components/Navigation";
 import CharacterCards from "../components/CharacterCards";
+import BattletagAndInfo from "../components/BattletagAndInfo";
+
 import Auth from "../utils/auth";
 
 const Dashboard = () => {
@@ -38,20 +41,15 @@ const Dashboard = () => {
                 <Navigation />
             </Jumbotron>
 
-            <Container fluid>
-                <h1 className="display-3">Your battletag, {profile.battleTag} </h1>
-                <p className="lead">Paragon Level: {profile.paragonLevel}  </p>
-                <p className="lead">Hardcore Paragon Level: {profile.paragonLevelHardcore}  </p>
-                <p className="lead">Season Paragon Level: {profile.paragonLevelSeason}  </p>
-                <p className="lead">Hardcore Season Paragon Level: {profile.paragonLevelSeasonHardcore}  </p>
-                <p className="lead">Guild: {profile.guildName} </p>
-            </Container>
+            <BattletagAndInfo profile={profile}/>
+
             <div className="row justify-content-center">
                 {profile.heroes ?
                     <CharacterCards heroes={profile.heroes} />
                     : <p>It looks like you don't have a battletag entered into your account. Click below to go and add a profile!</p>
                 }
             </div>
+
             <Jumbotron>
 
             </Jumbotron>
