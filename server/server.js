d65e1094257f96108f645ef378b5c955f6cecd63
neilburt <neil.burt@comcast.net>
2021-08-23 14:45:43 -0500

    I added a period.

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 db.once('open', () => {
