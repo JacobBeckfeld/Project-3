ff58a5e14b9667f836f646d281b5fc1951a77061
neilburt <neil.burt@comcast.net>
2021-08-24 00:49:10 -0500

    buttons smaller

--- client/src/components/BattletagAndInfo.js
+++ client/src/components/BattletagAndInfo.js
@@ -1,37 +1,9 @@
-import { React, useState } from "react";
+import React from "react";
 import { withRouter } from 'react-router-dom';
-import { Container, Button, Alert } from 'reactstrap';
-import { saveProfile } from "../utils/API";
-import Auth from '../utils/auth'
+import { Container } from 'reactstrap';
 
 const BattletagAndInfo = (props) => {
-    const [showAlert, setShowAlert] = useState(false);
     const profile = props.profile;
-    const _id = Auth.getProfile().data._id
-    const battletag = profile.battleTag;
-
-    const onDismiss = () => setShowAlert(false);
-
-    const handleSaveProfile = async (_id, battletag) => {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-            return false;
-        }
-
-        try {
-            const response = await saveProfile(_id, battletag)
-
-            if (response.ok) {
-                console.log(response)
-                setShowAlert(true)
-            } else {
-                alert('Failed to save profile');
-            }
-        } catch (err) {
-            console.error(err)
-        }
-    };
 
     return (
         <Container fluid>
@@ -41,10 +13,6 @@ const BattletagAndInfo = (props) => {
             <p className="lead">Season Paragon Level: {profile.paragonLevelSeason}  </p>
             <p className="lead">Hardcore Season Paragon Level: {profile.paragonLevelSeasonHardcore}  </p>
             <p className="lead">Guild: {profile.guildName} </p>
-            <Button onClick={() => handleSaveProfile(_id, battletag)} className="save-btag">Save Battletag</Button>
-            <Alert className="save-btag-alert" color="info" isOpen={showAlert} toggle={onDismiss}>
-                BattleTag saved!
-            </Alert>
         </Container>
     );
 }
